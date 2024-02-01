11a6e1102776029cbe93bfdd7e8036ec3810e34c
Krishna Sanjay Ghuge <120237516+krishna0729@users.noreply.github.com>
2024-02-01 15:41:58 +0530

    Add files via upload
    Here is how we implement different types of datatypes in python like float, integer, boolean, complex number etc.

---
+++ Datatypeinpython.py
@@ -0,0 +1,16 @@
+##study the implemantation of Data Types
+
+a=100
+print("int=",a)
+
+k=34.7
+print("float=",k)
+
+c=12+5j
+print("complex no=",c)
+
+p={11,12,13}
+print("set ele=",p)
+
+d={'aa':11,'bb':12,'cc':33}
+print("Dictionary ele =",d)
